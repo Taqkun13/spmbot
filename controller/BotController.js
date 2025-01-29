9a81ba714e2f61df7677889dd6030e92949dc50f
Taqkun13 <th596540@gmail.com>
2025-01-29 07:20:05 +0700

    Versi 0.2

--- controller/BotController.js
+++ controller/BotController.js
@@ -4,6 +4,7 @@ const f = require("../utils/Formatter");
 const { join } = require('path');
 const fs = require("fs");
 const path = require("path");
+const moment = require('moment-timezone');
 
 module.exports = class BotController extends Controller {
   constructor(request) {
@@ -59,11 +60,20 @@ module.exports = class BotController extends Controller {
                     console.error(updateErr);
                     return this.reply("Waduh... maaf aku lagi bingung🙏😵, silakan coba lagi ya...");
                   } else {
+                    const fileName = row.nik + request.text;
+                    const filePath = `public/${request.text}/${fileName}.pdf`;
                     // Periksa apakah file ada
                     if (fs.existsSync(filePath)) {
                       // Jika file ada, kirim file dan pesan konfirmasi
                       this.reply(Response.document.fromURL(filePath, 'application/pdf', ""));
                       this.reply("Ini ya slip gaji kamu periode ini...\nKalau ada pertanyaan masalah gaji bisa datang ke kantor SPM ya..\nTerima kasih😊🙏");
+                      
+                      // Waktu saat ini dalam format WIB
+                      const currentTimeWIB = moment().tz('Asia/Jakarta');  // Menggunakan timezone Asia/Jakarta untuk WIB
+                      // Waktu 7 jam yang lalu di WIB
+                      const timeThresholdWIB = currentTimeWIB.subtract(7, 'hours').format('YYYY-MM-DD HH:mm:ss');
+                      // Query untuk menghapus data yang lebih dari 7 jam
+                      db.run('DELETE FROM user_history WHERE time < ?', [timeThresholdWIB]);
                     } else {
                       // Jika file tidak ada, kirim pesan pemberitahuan
                       this.reply(`Maaf, slip gaji kamu periode ini belum tersedia. Silakan cek kembali nanti atau hubungi kantor SPM untuk informasi lebih lanjut. 😊🙏`);
@@ -189,20 +199,53 @@ module.exports = class BotController extends Controller {
             }
           }
         }else{
-          this.reply(Response.image.fromURL("https://i.ibb.co.com/jbJhJsS/spmbotlogo.webp", `*Halo ${request.name}🖐!* \nSelamat datang di SPMBOT, senang bisa berjumpa denganmu❤`))
-          .then(() => {
-            // Kirim balasan kedua setelah balasan pertama selesai
-            return this.reply(
-              Response.menu.fromArrayOfString(
-                [
-                  f("menu.tentangKantor"),
-                  f("menu.daftarRekening"),
-                  f("menu.slipGaji")
-                ],
-                f("intro", [request.name]),
-                f("template.menu")
-              )
-            );
+          const number = request.number;
+          this.db.get(
+            `SELECT * FROM user_history WHERE id = ?`,
+            [number],
+            (err, row) => {
+              if(row){
+                return this.reply(
+                  Response.menu.fromArrayOfString(
+                    [
+                      f("menu.tentangKantor"),
+                      f("menu.daftarRekening"),
+                      f("menu.slipGaji")
+                    ],
+                    f("intro", [request.name]),
+                    f("template.menu")
+                  )
+                );
+              }else{
+                const now = new Date();
+                // Mendapatkan UTC offset dalam menit (misalnya untuk WIB adalah 7 jam atau 420 menit)
+                const wibOffset = 7 * 60;
+                // Mengubah waktu UTC ke WIB
+                now.setMinutes(now.getMinutes() + wibOffset);
+                // Menampung waktu WIB dalam format YYYY-MM-DD HH:mm:ss
+                const time = now.toISOString().replace('T', ' ').slice(0, 19);
+                this.db.run(
+                  `INSERT INTO user_history (id, time) VALUES (?, ?)`,
+                  [number, time],
+                  (err) => {
+                    this.reply(Response.image.fromURL("public/spmbotlogo.webp", `*Halo ${request.name}🖐!* \nSelamat datang di SPMBOT, senang bisa berjumpa denganmu❤`))
+                    .then(() => {
+                      // Kirim balasan kedua setelah balasan pertama selesai
+                      return this.reply(
+                        Response.menu.fromArrayOfString(
+                          [
+                            f("menu.tentangKantor"),
+                            f("menu.daftarRekening"),
+                            f("menu.slipGaji")
+                          ],
+                          f("intro", [request.name]),
+                          f("template.menu")
+                        )
+                      );
+                    })
+                  }
+                )
+              }
           })
         }
       }
@@ -211,14 +254,14 @@ module.exports = class BotController extends Controller {
 
   // Function untuk memberikan informasi tentang kantor
   async tentangKantor(request) {
-    await this.reply(Response.image.fromURL("https://i.ibb.co.com/kgKqWKt/spm-office-intro.png", "*PT. SIANTAR PUTRA MANDIRI (PT.SPM)* merupakan perusahaan outsourching yang menyediakan jasa penyalur tenaga kerja ke barbagai perusahaan yang bekerja sama dengan kami"));
-    await this.reply(Response.image.fromURL("https://i.ibb.co.com/W58nSRf/pt-siantar-putra-mandiri.jpg", "PT. SIANTAR PUTRA MANDIRI berlokasi di Jl. Masjid Sukorejo No.60, RT.10/RW.03, Sukorejo, Lolawang, Kec. Ngoro, Kabupaten Mojokerto, Jawa Timur 61385"));
+    await this.reply(Response.image.fromURL("public/officeIntro.png", "*PT. SIANTAR PUTRA MANDIRI (PT.SPM)* merupakan perusahaan outsourching yang menyediakan jasa penyalur tenaga kerja ke barbagai perusahaan yang bekerja sama dengan kami"));
+    await this.reply(Response.image.fromURL("public/officePhoto.jpg", "PT. SIANTAR PUTRA MANDIRI berlokasi di Jl. Masjid Sukorejo No.60, RT.10/RW.03, Sukorejo, Lolawang, Kec. Ngoro, Kabupaten Mojokerto, Jawa Timur 61385"));
     await this.reply("Google Maps: https://maps.app.goo.gl/TVmjRcvApQxBoUgi6");
   }
 
   // Function untuk menampilkan informasi pendaftaran rekening
   async daftarRekening(request) {
-    return Response.image.fromURL("https://i.ibb.co.com/9WWp5xW/QR-ATM.png", "*Kamu ingin melakukan pendaftaran rekening ya...*\n\nKalo gitu isi formulir pada link dibawah ini dengan benar dan lengkap atau scan barcode diatas yaa..\nhttps://forms.gle/rpMdtMLB9wHGV9AS7");
+    return Response.image.fromURL("public/qr.png", "*Kamu ingin melakukan pendaftaran rekening ya...*\n\nKalo gitu isi formulir pada link dibawah ini dengan benar dan lengkap atau scan barcode diatas yaa..\nhttps://forms.gle/rpMdtMLB9wHGV9AS7");
   }
 
   // Function untuk menangani permintaan slip gaji
